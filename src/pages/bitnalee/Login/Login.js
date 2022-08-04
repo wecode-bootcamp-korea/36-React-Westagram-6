db5ed598bcc216898ba7b97e1d89d3d3c2a5d67f
bitnaleeeee <bitnaleeeee@gmail.com>
2022-08-04 16:24:50 +0900

    Add : Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/bitnalee/Login/Login.js
+++ src/pages/bitnalee/Login/Login.js
@@ -2,20 +2,20 @@ import React, { useState } from 'react';
 import './Login.scss';
 import { Link, Navigate } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import { isDisabled } from '@testing-library/user-event/dist/utils';
 
 const LoginBitna = () => {
   const [id, idValue] = useState('');
   const [pw, pwValue] = useState('');
+  const [val, setVal] = useState(true);
 
+  // 아아디 비밀번호 데이터 전달 함수
   function saveUserld(e) {
-    if (e.target.id === 'id') {
-      idValue(e.target.value);
-      console.log(id);
-    } else {
-      pwValue(e.target.value);
-      console.log(pw);
-    }
+    e.target.id === 'id' ? idValue(e.target.value) : pwValue(e.target.value);
+    // 아이디 비밀번호 기준 충족시, 로그인 버튼 활성화 시킬 함수
+    id.length > 5 && pw.length > 5 ? setVal(false) : setVal(true);
   }
+
   return (
     <div className="login">
       <article className="article">
@@ -33,7 +33,7 @@ const LoginBitna = () => {
             id="pw"
             placeholder="비밀번호"
           />
-          <button type="button" id="button">
+          <button disabled={val} type="button" id="button">
             로그인
           </button>
         </div>
