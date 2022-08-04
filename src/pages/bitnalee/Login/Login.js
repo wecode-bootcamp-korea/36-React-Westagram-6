d303240a3a517022e62ace65f921b27d4b799f2d
bitnaleeeee <bitnaleeeee@gmail.com>
2022-08-04 15:04:28 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/bitnalee/Login/Login.js
+++ src/pages/bitnalee/Login/Login.js
@@ -4,43 +4,36 @@ import { Link, Navigate } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 
 const LoginBitna = () => {
-  const [val, setVal] = useState('비활성');
   const [id, idValue] = useState('');
   const [pw, pwValue] = useState('');
-  function saveUserld(e) {
-    // 버튼 활성화 함수
-    idValue(e.target.value);
-    pwValue(e.target.value);
 
-    if (idValue.length >= 5 && pwValue.length >= 5) {
-      setVal('활성');
+  function saveUserld(e) {
+    if (e.target.id === 'id') {
+      idValue(e.target.value);
+      console.log(id);
     } else {
-      setVal('비활성');
+      pwValue(e.target.value);
+      console.log(pw);
     }
   }
-  // 클릭시 메인페이지 이동 함수
-  const navigate = useNavigate();
-  const goToMain = () => {
-    navigate('/main');
-  };
-
   return (
     <div className="login">
       <article className="article">
         <div className="logo">westargram</div>
         <div className="inputBox">
           <input
+            onChange={saveUserld}
             type="text"
             id="id"
             placeholder="전화번호, 사용자 이름 또는 이메일"
           />
-          <input type="password" id="pw" placeholder="비밀번호" />
-          <button
-            type="button"
-            id="button"
-            disabled={val == '활성' ? false : true}
-            onClick={goToMain}
-          >
+          <input
+            onChange={saveUserld}
+            type="password"
+            id="pw"
+            placeholder="비밀번호"
+          />
+          <button type="button" id="button">
             로그인
           </button>
         </div>
