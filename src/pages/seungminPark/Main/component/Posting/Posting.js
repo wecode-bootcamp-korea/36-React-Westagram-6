4332e18421bd057f5e13759646b52b833b6bdd8d
pmb087 <pmb087@gmail.com>
2022-08-09 17:04:07 +0900

    Add: Feed With Mock Data

--- src/pages/seungminPark/Main/component/Posting/Posting.js
+++ src/pages/seungminPark/Main/component/Posting/Posting.js
@@ -1,13 +1,17 @@
-import React, { useState } from 'react';
-import AppendCommnets from './component/AppendComments/AppendComments';
+import React, { useEffect, useState } from 'react';
+import PostFeed from './component/PostFeed/PostFeed';
 import './Posting.scss';
 
 const Posting = () => {
-  const [postLike, setPostLike] = useState(false);
+  const [feedList, setFeedList] = useState([]);
 
-  const likePost = event => {
-    if (event.target.id === 'heart') setPostLike(!postLike);
-  };
+  useEffect(() => {
+    fetch('/data/seungminFeed.json')
+      .then(res => res.json())
+      .then(data => {
+        setFeedList(data);
+      });
+  }, []);
 
   return (
     <article className="posting">
@@ -17,50 +21,18 @@ const Posting = () => {
           <div className="postNewProfileId">lofi_image</div>
         </div>
       </section>
-      <section className="postFeed">
-        <div className="postFeedHeader">
-          <div className="feedHeaderProfile">
-            <div className="feedHeaderIcon" />
-            <div className="feedHeaderId">lofi_image</div>
-          </div>
-          <div className="feedHeaderFunction">
-            <i className="fa-solid fa-ellipsis" />
-          </div>
-        </div>
-        <div className="feedImage" />
-        <section className="feedFunctionSection">
-          <div className="feedFunctions">
-            <div className="feedFunctionContents">
-              <div className="feedFunctionContentsItems">
-                <i
-                  className={
-                    postLike
-                      ? 'fa-solid fa-heart fa-xl postHeart postLike'
-                      : 'fa-regular fa-heart fa-xl postHeart'
-                  }
-                  id="heart"
-                  onClick={likePost}
-                />
-              </div>
-              <div className="feedFunctionContentsItems">
-                <i className="fa-regular fa-comment fa-xl" />
-              </div>
-              <div className="feedFunctionContentsItems">
-                <i className="fa-solid fa-paper-plane fa-lg" />
-              </div>
-            </div>
-            <div className="feedFunctionClip">
-              <i className="fa-regular fa-bookmark fa-lg" />
-            </div>
-          </div>
-        </section>
-        <div className="feedLike">좋아요 1,897개</div>
-        <div className="feedAuthor">
-          <span>lofi_image</span>
-          <span>what a wonderful picture!!</span>
-        </div>
-        <AppendCommnets />
-      </section>
+      {feedList.map(data => {
+        return (
+          <PostFeed
+            key={data.id}
+            userId={data.userId}
+            profileImg={data.profileImg}
+            feedContents={data.feedContents}
+            feedImg={data.feedImg}
+            feedlikes={data.feedlikes}
+          />
+        );
+      })}
     </article>
   );
 };
